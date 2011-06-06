ae6b313b35def14c511c1c5ec129aac31236ae73
Techniki Multimedialne <techniki.multimedialne@gmail.com>
2011-06-07 00:38:03 +0200

    Rejestracja usera, walidacja, logout

--- urls.py
+++ urls.py
@@ -1,8 +1,19 @@
+import os
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
 from bookmarks.views import *
 
-
+site_media = os.path.join(
+    os.path.dirname(__file__), 'site_media'
+)
 urlpatterns = patterns('',
     (r'^$', main_page),
     (r'^user/(\w+)/$', user_page),
+    (r'^login/$', 'django.contrib.auth.views.login'),
+    (r'^logout/$', logout_page),
+    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', 
+        {'document_root': site_media}),
+    (r'^register/$', register_page),
+    (r'^register/success/$', direct_to_template, 
+        {'template': 'registration/register_success.html'}),
 )
